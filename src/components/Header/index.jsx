9ed0e29450dbedb3b448b2878a8c119866567b9c
Susanne Krkoska <zuzka.krkoska@seznam.cz>
2023-05-25 14:36:27 +0200

    Header WIP, global styles changed, fonts linked

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css"
+import {Link} from "react-router-dom";
+
+export const Header = () => {
+    return  (
+        <header>
+        <h1 className="app-logo">Neztrať hlavu!</h1>
+            <div className="slogan">Úklid nemusí být otrava... ale může!</div>
+            <nav>
+                <Link className="navlink" to={"/about"}>O nás</Link>
+            </nav>
+        </header>
+    )
+
+}
+
+
