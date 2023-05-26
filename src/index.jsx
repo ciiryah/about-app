059a36c0504505970b479588a37179c350c200a6
Kamila Semotánová <kamila@Kamilas-MacBook-Air.local>
2023-05-26 14:56:18 +0200

    Added footer

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
-import {Header} from "./components/Header";
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 const App = () => {
   return (
@@ -11,36 +12,35 @@ const App = () => {
       <main>
         <p>
           Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
+          <a href="https://www.npmjs.com/package/create-czechitas-app">
+            create-czechitas-app
+          </a>
           .
         </p>
       </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
+      <Footer />
     </div>
   );
 };
 
 const router = createBrowserRouter([
-    {
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <App />, //přepsat na <ErrorPage /> až bude hotová
+    children: [
+      {
         path: '/',
-        element: <App/>,
-        errorElement: <App/>, //přepsat na <ErrorPage /> až bude hotová
-        children: [
-            {
-                path: '/',
-                element: <App />, //přepsat na <HomePage /> až bude hotová
-            },
-            {
-                path: 'about',
-                element: <App />, //přepsat na <AboutPage /> až bude hotová
-            },
-        ],
-    },
+        element: <App />, //přepsat na <HomePage /> až bude hotová
+      },
+      {
+        path: 'about',
+        element: <App />, //přepsat na <AboutPage /> až bude hotová
+      },
+    ],
+  },
 ]);
 
-
-createRoot(
-  document.querySelector('#app'),
-).render(<RouterProvider router={router} />);
+createRoot(document.querySelector('#app')).render(
+  <RouterProvider router={router} />,
+);
