c7d0babe2c745fac044154dd99f0d00860ecc080
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-05-28 12:51:25 +0200

    base of AboutPage added

--- src/pages/AboutPage/index.jsx
+++ src/pages/AboutPage/index.jsx
@@ -9,7 +9,20 @@ export const AboutPage = () => {
       <Header />
       <main>
         <div>
-          <p>O nás o nás o nás</p>
+          <h2 className="title">O nás</h2>
+          <p className="mainText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo doloribus similique mollitia velit quod exercitationem maiores ipsa dolorem nulla cumque, possimus numquam. Itaque beatae, quasi ut accusamus reprehenderit sunt nulla!</p>
+          <div className="medailons">
+            <div className="medailon">
+              <img src="" alt="Zuzka" />
+              <h4>Susanne Krkoška</h4>
+              <button className="moreInfoBtn">Více o mě</button>
+            </div>
+            <div className="medailon">
+              <img src={require("./img/kamila.jpeg")} alt="Kamča" className="picture"/>
+              <h4>Kamila Semotánová</h4>
+              <button className="moreInfoBtn">Více o mě</button>
+            </div>
+          </div>
         </div>
       </main>
       <Footer />
