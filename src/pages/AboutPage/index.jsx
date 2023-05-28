2edfe2b58f6e84539e6c07d6d92e98256c484482
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-05-28 23:34:40 +0200

    mainText content generated

--- src/pages/AboutPage/index.jsx
+++ src/pages/AboutPage/index.jsx
@@ -25,14 +25,20 @@ export const AboutPage = () => {
         <div>{ !click && (
           <>
             <h2 className="title">O nás</h2>
-            <p className="mainText">Dvě holky, které mají za sebou studium VŠCHT. To jsme my. Sdílené trauma ze studia nás už v první moment spojilo.</p>
-            <p>Společně si tvoříme realistické, ale dostatečně ambiciózní plány.</p>
+            <div className="mainText">
+              <p>Jsme dvě holky, které mají za sebou studium VŠCHT. Sdílené trauma ze studia nás už při prvním setkání spojilo.</p> 
+              <p>Osud nám sice nedopřál splnit si naše studijní plány, ale to nás nezastavilo v hledání nových výzev a rozvoje. Naše studium na VŠCHT nám dalo pevný základ v technických a analytických dovednostech, které jsou neocenitelné při vývoji webových aplikací. I když jsme nedostudovali, neztratili jsme vášeň a zápal pro svou práci.</p>
+              <p>Společně tvoříme dynamický a nadšený tým, který si tvoří realistické, ale dostatečně ambiciózní plány.</p>
+            </div>
             <div className="medailons">
               <div className="medailon">
-                <img src={require("./img/susanne.jpg")} alt="Zuzka" className="picture" />
+                <img 
+                  src={require("./img/susanne.jpg")} alt="Zuzka" 
+                  className="picture" />
                 <h4>Susanne Krkoška</h4>
-                <button className="btn moreInfoBtn"
-                onClick={() => {handleClick("zuzka")}}>Více o mě</button>
+                <button 
+                  className="btn moreInfoBtn"
+                  onClick={() => {handleClick("zuzka")}}>Více o mě</button>
               </div>
               <div className="medailon">
                 <img src={require("./img/kamila.jpeg")} alt="Kamča" className="picture"/>
